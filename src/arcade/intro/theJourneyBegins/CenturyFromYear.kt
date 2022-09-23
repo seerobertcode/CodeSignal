6b4bbe3a60df5bfb074df42439047e8ec3d57a60
seerobertcode <robert.bosoiu@yahoo.com>
2022-09-23 11:26:32 +0300

    Arcade Intro The Journey Begins - centuryFromYear

--- src/arcade/intro/theJourneyBegins/CenturyFromYear.kt
+++ src/arcade/intro/theJourneyBegins/CenturyFromYear.kt
@@ -1,9 +1,12 @@
 package arcade.intro.theJourneyBegins
 
 fun main() {
-    println("Century from year : ${solution(1998)}")
+    println("Century from year : ${solution(1700)}")
 }
 
 fun solution(year: Int): Int {
-
+    if (year % 100 != 0)
+        return year / 100 + 1
+    else
+        return year / 100
 }
